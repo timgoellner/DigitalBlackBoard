1f9bc3664c28c02c2c272cc37e960cc18e69852e
SirRoti <timgoellner6@gmail.com>
2024-07-21 23:47:47 +0200

    Add login page styling

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
+import "../styles/Login.css"
+
 function Login() {
   const [organization, setOrganization] = useState('')
   const [identifier, setIdentifier] = useState('')
@@ -36,56 +38,69 @@ function Login() {
       })
   }
 
+  function toggleLogin() {
+    document.getElementsByClassName('switch')[0].classList.toggle('clicked')
+    document.getElementsByClassName('forms')[0].classList.toggle('clicked')
+  }
+
   return (
     <main>
-      <form onSubmit={(event) => {
-        isStaff = false
-        submitUser(event);
-      }}>
-        <input
-          value={organization}
-          type="text"
-          placeholder="Organization"
-          onChange={(e) => setOrganization(e.target.value)}
-        />
+      <h1>DigitalBlackBoard Login</h1>
+      <div className='switch' onClick={(event) => { toggleLogin(event) }}></div>
+      <div className='forms'>
+        <form onSubmit={(event) => {
+          isStaff = false
+          submitUser(event);
+        }}>
+          <h2>User Login</h2>
+          <input
+            value={organization}
+            type="text"
+            placeholder="Organization"
+            onChange={(e) => setOrganization(e.target.value)}
+          />
 
-        <input
-          value={password}
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+            value={password}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <button type="submit">Login</button>
-      </form>
+          <button type="submit">Login</button>
+        </form>
 
-      <form onSubmit={(event) => {
-        isStaff = true
-        submitUser(event);
-      }}>
-        <input
-          value={organization}
-          type="text"
-          placeholder="Organization"
-          onChange={(e) => setOrganization(e.target.value)}
-        />
+        <div className='gap'></div>
 
-        <input
-          value={identifier}
-          type="text"
-          placeholder="Identifier"
-          onChange={(e) => setIdentifier(e.target.value)}
-        />
+        <form onSubmit={(event) => {
+          isStaff = true
+          submitUser(event);
+        }}>
+          <h2>Staff Login</h2>
+          <input
+            value={organization}
+            type="text"
+            placeholder="Organization"
+            onChange={(e) => setOrganization(e.target.value)}
+          />
 
-        <input
-          value={password}
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <input
+            value={identifier}
+            type="text"
+            placeholder="Identifier"
+            onChange={(e) => setIdentifier(e.target.value)}
+          />
 
-        <button type="submit">Login</button>
-      </form>
+          <input
+            value={password}
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </main>
   );
 }
