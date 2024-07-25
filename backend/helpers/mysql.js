d46f6faef6382a2dc57955f7cba71f15b203ee8e
SirRoti <timgoellner6@gmail.com>
2024-07-25 20:25:09 +0200

    add database structure

--- backend/helpers/mysql.js
+++ backend/helpers/mysql.js
@@ -1,5 +1,7 @@
 /*
-DATABASE COLUMN SETUP
+DATABASE TABLE SETUP
+
+|| BASICS
 
  * accounts
     - id (int [pk, nn, uq, ai])
@@ -7,6 +9,61 @@ DATABASE COLUMN SETUP
     - identifier (varchar(45) [nn])
     - password (varchar(45))
     - isStaff (boolean [nn])
+
+ * grades
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - grade (varchar(5) [nn])
+    - subgrade (varchar(1))
+    - hasSubgrade (boolean [nn])
+
+ * teachers
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - forename (varchar(45) [nn])
+    - lastname (varchar(45) [nn])
+
+ * students
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - forename (varchar(45) [nn])
+    - lastname (varchar(45) [nn])
+    - grade (int [fk, nn])
+
+ * classes
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - teacher (int [fk, nn])
+    - subject (int [fk, nn])
+    - room (int [fk, nn])
+    - grade (int [fk])
+    - hasGrade (boolean [nn])
+
+|| CONNECTIONS
+
+ * student_classes
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - student (int [fk, nn])
+    - class (int [fk, nn])
+
+ * teacher_subjects
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - teacher (int [fk, nn])
+    - subject (int [fk, nn])
+
+|| SMALLS
+
+ * subjects
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - name (varchar(45) [nn])
+
+ * rooms
+    - id (int [pk, nn, uq, ai])
+    - organization (varchar(45) [nn])
+    - name (varchar(45) [nn])
 */
 
 if (process.env.NODE_ENV !== "production") { require("dotenv").config() }
