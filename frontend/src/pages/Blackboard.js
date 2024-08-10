9066d4f95721124015238876230cd1cc4fae7c0e
SirRoti <timgoellner6@gmail.com>
2024-08-10 23:51:29 +0200

    add blackboard positioning logic

--- frontend/src/pages/Blackboard.js
+++ frontend/src/pages/Blackboard.js
@@ -1,15 +1,21 @@
 import { useNavigate, useLoaderData } from 'react-router-dom';
 import { useState, useMemo } from 'react';
 
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+
 import "../styles/Blackboard.css"
 
 function Blackboard() {
-  const [blackboard, setBlackboard] = useState()
+  const [blackboard, setBlackboard] = useState([[], [], [], [], [], [], []])
   const [student, setStudent] = useState()
+  const [timelines, setTimelines] = useState([])
 
   const navigate = useNavigate()
   const user = useLoaderData()
   const token = localStorage.getItem('jwt-token')
+
+  const weekdays = { "monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6, "sunday": 7 }
 
   const refresh = () => {
     fetch(`http://localhost:100/blackboard/${user.organization}/${user.identifier}`, { headers: { 'jwt-token': token } })
@@ -18,16 +24,40 @@ function Blackboard() {
         if (data.message !== 'success') navigate('/login')
 
         var blackboardData = data.blackboardData
+        var blackboardHtml = [[], [], [], [], [], [], []]
 
+        blackboardData.forEach((element) => {
+          const index = weekdays[element.weekday]-1
 
-        blackboardData = blackboardData.map((class_) => {
-          return (
-            <p key={class_.id}>{class_.name}</p>
-          )
+          blackboardHtml[index] = [
+            ...blackboardHtml[index],
+            <div className='element' style={{ height: `${((element.endTime - element.startTime) / (data.latest - data.earliest)) * 70}dvh`, top: `${((element.startTime - data.earliest) / (data.latest - data.earliest)) * 70}dvh` }}>
+              <p>{String((element.startTime / 60) | 0).padStart(2, '0')}:{String(element.startTime % 60).padStart(2, '0')}</p>
+              <div className='data'>
+                <p>{element.name}</p>
+              </div>
+              <p>{String((element.endTime / 60) | 0).padStart(2, '0')}:{String(element.endTime % 60).padStart(2, '0')}</p>
+            </div>
+          ]
         })
 
-        setBlackboard(blackboardData)
+        var timelinesHtml = []
+        const timelinesCount = ((data.latest / 60) | 0) - ((data.earliest / 60) | 0)
+        const topOffset = (1 - (data.earliest / 60 % 1))
+        for (var i = 1; i <= timelinesCount; i++) {
+          timelinesHtml.push(
+            <div className='timeline' style={{ height: `${70/timelinesCount}dvh`, marginTop: `${(i === 1) ? ((70/timelinesCount) * topOffset) : 0}dvh`}}>
+              <span>
+                <p>{parseInt((data.earliest / 60) | 0) + i}</p>
+                <div></div>
+              </span>
+            </div>
+          )
+        }
+
+        setBlackboard(blackboardHtml)
         setStudent(data.student)
+        setTimelines(timelinesHtml)
       })
   }
 
@@ -40,15 +70,35 @@ function Blackboard() {
 
   return (
     <>
-      <div className='blackboard'>
+      <div className='blackboard-page'>
         <header>
-          <p>{user.organization}</p>
+          <p>{user.organization}<br />{student?.forename} {student?.lastname}</p>
           <h1>Blackboard</h1>
           <button onClick={() => logout()}>Logout</button>
         </header>
         <main className='content'>
-         <p>{student.forename} {student.lastname}</p> 
-          {blackboard}
+          <div className='buttons'>
+            <button><IoIosArrowBack /></button>
+            <button><IoIosArrowForward /></button>
+          </div>
+          <div>
+            <div className='blackboard'>
+              {blackboard.map((weekday, index) => {
+                return ((weekday.length > 0) && (
+                  <div className='weekday'>
+                    <p>{Object.keys(weekdays).find((_, dayIndex) => dayIndex === index)}</p>
+                    <div>
+                      <div className='timelines'>
+                        {timelines}
+                      </div>
+                      
+                      {weekday}
+                    </div>
+                  </div>
+                ))
+              })}
+            </div>
+          </div>
         </main>
       </div>
     </>
